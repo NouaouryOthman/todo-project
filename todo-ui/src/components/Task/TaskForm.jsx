dfa262b5e2cb251054d2e1be9ccbd463bc5f9712
NOUAOURY <othman.nouaoury@capgemini.com>
2025-12-03 17:41:53 +0100

    refactor: fix reload when adding new task

--- todo-ui/src/components/Task/TaskForm.jsx
+++ todo-ui/src/components/Task/TaskForm.jsx
@@ -3,12 +3,22 @@ import { addTask } from "../../services/task.service";
 
 export default function TaskForm({ onSubmit }) {
   const [formData, setFormData] = useState({ title: "", description: "" });
+  const [isAdding, setIsAdding] = useState(false);
 
   const handleSubmit = (event) => {
+    console.log(isAdding);
+    setIsAdding(true);
+    console.log(isAdding);
     const newTask = { ...formData };
     event.preventDefault();
 
-    addTask(newTask).then(({ data }) => onSubmit({ ...data }));
+    addTask(newTask).then(({ data }) => {
+      onSubmit({ ...data })
+      setIsAdding(false)
+    }).catch(error => {
+      console.error(`Failed to add Task: ${error}`);
+      setIsAdding(false);
+    })
   };
 
   return (
@@ -28,7 +38,7 @@ export default function TaskForm({ onSubmit }) {
           setFormData({ ...formData, description: e.target.value })
         }
       />
-      <button type="submit" className="text-green-600 text-sm">
+      <button type="submit" className="text-green-600 text-sm disabled:text-gray-500" disabled={isAdding}>
         Add Task
       </button>
     </form>
