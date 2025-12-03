6b983a9f3c09cca1079e85997ce89dfe5dd28e49
NOUAOURY <othman.nouaoury@capgemini.com>
2025-12-03 11:25:29 +0100

    Fix: cors error

--- todo-ui/vite.config.js
+++ todo-ui/vite.config.js
@@ -15,5 +15,12 @@ export default defineConfig({
     origin: "http://localhost:3000",
     allowedHosts: true,
     cors: true,
+
+    proxy: {
+      "/": {
+        target: import.meta.env.VITE_API_BASE_URL,
+        changeOrigin: true,
+      },
+    },
   },
 });
